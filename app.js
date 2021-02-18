ad1ab011909db90a22bc7ee582045cc01cabc225
ChillyWilly <noreply>
2021-02-18 01:07:00 -0500

    user input with inquirer

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+const inquirer = require('inquirer');
 // sets the filesystem module from node.js standard library to the const fs so that the module can be used
-const fs = require('fs');
-// same as fs but for our own file to generate the page template and insert name and github link
-const generatePage = require('./src/page-template.js');
-// get the command line arguments starting from index 2 until the end (use .length because backend is exclusive)
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// notation to set variable names for corresponding indices in array
-const [name, github] = profileDataArgs;
-// writeFile takes 3 arguments name of file being written to, what to write, function in case of error
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// const fs = require('fs');
+// // same as fs but for our own file to generate the page template and insert name and github link
+// const generatePage = require('./src/page-template.js');
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-}); 
+// // writeFile takes 3 arguments name of file being written to, what to write, function in case of error
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
 
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// }); 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => {
+    console.log(answers);
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+    // prompt couldn't be rendered in the current environment
+  } else {
+
+  }
+});
