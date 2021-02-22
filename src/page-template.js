9df49df32645ae98601d6d4eb1a9c7de4b3e2b5d
ChillyWilly <noreply>
2021-02-22 18:11:03 -0500

    refactor to promise based async

--- src/page-template.js
+++ src/page-template.js
@@ -3,7 +3,7 @@ const generateAbout = aboutText => {
     return '';
   }
   return `
-    <section class="my-3 id="about">
+    <section class="my-3 id=about">
       <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
       <p>${aboutText}</p>
       </section>
